893f2d661a3179e166aa78c553b9bb7c1187dc3a
ankushKun <ankush4singh@gmail.com>
2024-08-10 05:24:15 +0530

    checkpoint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <ThemeProvider
+    attribute="class"
+    defaultTheme="light"
+  ><Component {...pageProps} /></ThemeProvider>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,183 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import { Inter } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ["latin"] });
+import { FaDiscord, FaGreaterThan, FaLessThan, FaLinkedin, FaTwitter, FaXTwitter } from "react-icons/fa6"
+import { TbMailFilled } from "react-icons/tb"
+
+import pattern from "@/assets/pattern.svg";
+import ide from "@/assets/ide.png";
+import apm from "@/assets/apm.png";
+import apmcli from "@/assets/apmcli.png";
+import codecell from "@/assets/codecell.png";
+import logo from "@/assets/logo.png";
+import mirror from "@/assets/mirror.png";
+import logoFull from "@/assets/logo-full.png";
+
+
+type TProduct = {
+  heading: string,
+  title: string,
+  description: string,
+  image: string | StaticImageData,
+  link: string
+}
+
+const products: TProduct[] = [
+  {
+    heading: "Web IDE",
+    title: "Web IDE for seamless DevX on AO",
+    description: "Build faster, smarter, and more efficiently with BetterIDEa, the ultimate native web IDE for AO development",
+    image: ide,
+    link: "https://ide.betteridea.dev"
+  },
+  {
+    heading: "AO Package Manager",
+    title: "Package Manager for AO",
+    description: "Discover, install, and manage packages with ease",
+    image: apm,
+    link: "https://apm.betteridea.dev"
+  },
+  {
+    heading: "LUA Code Cells",
+    title: "Portable Code Cells for AO",
+    description: "Run and share code snippets in any webapp",
+    image: codecell,
+    link: "https://www.npmjs.com/package/@betteridea/codecell"
+  },
+  {
+    heading: "APM CLI Tool",
+    title: "Command Line Interface for APM",
+    description: "Manage your AO packages from the terminal",
+    image: apmcli,
+    link: "https://www.npmjs.com/package/@betteridea/apm"
+  }
+]
+
+function Navbar() {
+  const items = [
+    { name: "Home", href: "#home" },
+    { name: "Products", href: "#products" },
+    { name: "Team", href: "#team" },
+    { name: "Contact", href: "#contact" },
+  ]
+
+  const latestActiveUrl = typeof window !== 'undefined' ? window.location.href : '';
+  const activeUrl = latestActiveUrl.split('#')[1];
+
+  return <div className="z-20 flex items-center justify-center gap-2 p-3 w-fit rounded-full border border-foreground/40 mx-auto fixed left-0 right-0 top-5 bg-background/70 backdrop-blur">
+    <Link href="https://docs.betteridea.dev" target="_blank">
+      <Button className="rounded-full px-4 h-7 data-[active=true]:bg-primary" variant="ghost">Docs</Button>
+    </Link>
+    <Image src={logo} alt="BetterIDEa" width={30} height={30} />
+    {
+      items.map(item => (
+        <Link href={item.href} key={item.name}>
+          <Button data-active={("#" + activeUrl).endsWith(item.href)} className="rounded-full px-4 h-7 data-[active=true]:bg-primary" variant="ghost">{item.name}</Button>
+        </Link>
+      ))
+    }
+  </div>
+}
+
+function Product({ heading, title, description, image, link }: {
+  heading: string,
+  title: string,
+  description: string,
+  image: string | StaticImageData,
+  link: string
+}) {
+  return <div className="mx-auto md:px-24">
+    <div className="text-3xl font-serif-display text-center flex gap-3 items-center relative p-5 px-10 md:px-20 pt-20">{heading} <div className="h-[1px] bg-foreground/30 grow" /></div>
+    <div className="mx-10 md:mx-20 p-4 bg-gradient-to-b from-foreground/5 to-white rounded-xl">
+      <div className="flex flex-col md:flex-row gap-5 items-center">
+        <div className="aspect-square">
+          <Image src={image} alt={title} width={500} height={500} />
+        </div>
+        <div className="md:w-1/2">
+          <div className="text-xl font-bold">{title}</div>
+          <div className="text-lg text-muted-foreground">{description}</div>
+          <Link href={link} target="_blank">
+            <Button className="mt-5 rounded-full text-foreground">Try it yourself <FaGreaterThan className="scale-x-50 ml-2" /></Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  </div>
+}
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <Head>
+        <title>BetterIDEa</title>
+        <meta name="description" content="BetterIDEa - Enhancing the AO developer experience" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="/logo.png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+      </Head>
+      <Navbar />
+      <div className="min-h-screen flex items-center" id="home">
+        <Image src={pattern} alt="bg-grid" className="w-full h-[220px] object-bottom object-cover absolute top-0 -z-10" draggable={false} />
+        <div className="mx-auto w-fit z-10">
+          <div className="flex flex-col gap-4 items-center font-serif-display text-4xl md:text-6xl">
+            <div>Build the future on <i>AO</i></div>
+            <div className="flex items-center gap-5">w/ <span className="text-primary text-5xl md:text-7xl">BetterIDEa</span></div>
+          </div>
+        </div>
+        <Image src={pattern} alt="bg-grid" className="w-full h-[220px] object-bottom object-cover absolute bottom-0 -z-10 rotate-180" draggable={false} />
+      </div>
+      <div id="products" className="relative bottom-24"></div>
+      <div className="bg-primary text-center p-5 text-2xl">A suite of tools to accelerate your development on AO</div>
+
+      {
+        products.map(product => <Product key={product.title} {...product} />)
+      }
+
+      <div className="mb-20" />
+      <div id="team" className="relative bottom-24"></div>
+      <div className="bg-primary text-center p-5 text-2xl">Embrace the Permaweb - Start building with BetterIDEa</div>
+
+      <div className="min-h-[70vh] flex flex-col items-center justify-center">
+        <div className="text-center text-4xl font-serif-display">The Team</div>
+        <div className="mx-auto w-fit flex flex-col items-center my-5">
+          <Image src="https://pbs.twimg.com/profile_images/1797158678032003072/kD_OsxAF_400x400.jpg" alt="Ankush" className="rounded-full border" width={120} height={120} draggable={false} />
+          <div className="text-3xl flex items-center gap-2">Ankush</div>
+          <div className="-mt-1 mb-1 text-muted-foreground flex items-center gap-2">Founder</div>
+          <div className="font-serif-display flex items-center gap-2 my-2">
+            <Link href="https://x.com/ankushKun_" target="_blank"><FaXTwitter size={20} /></Link>
+            <Link href="https://linkedin.com/in/ankushKun" target="_blank"><FaLinkedin size={20} /></Link>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div id="contact" className="relative bottom-24"></div>
+      <div className="min-h-[80vh] flex flex-col items-center justify-center">
+        <div className="text-center text-4xl font-serif-display">Follow us for latest updates!</div>
+        <div className="flex gap-2 justify-between my-10 p-1">
+          <Link href="https://x.com/betteridea_dev" target="_blank"><Button variant="ghost" className="h-16 rounded-full"><FaXTwitter size={30} /></Button></Link>
+          <Link href="https://discord.gg/nm6VKUQBrA" target="_blank"><Button variant="ghost" className="h-16 rounded-full"><FaDiscord size={30} /></Button></Link>
+          <Link href="https://linkedin.com/company/betteridea-dev" target="_blank"><Button variant="ghost" className="h-16 rounded-full"><FaLinkedin size={30} /></Button></Link>
+          <Link href="https://mirror.xyz/0xCf673b87aFBed6091617331cC895376209d3b923" target="_blank"><Button variant="ghost" className="h-16 w-16 rounded-full p-3"><Image src={mirror} width={100} height={100} alt="mirror-logo" /></Button></Link>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer className=" bg-foreground justify-between items-center text-background p-3 px-6 flex rounded-t">
+        <div>
+          <div className="mx-auto w-fit flex items-center gap-2">
+            <Image src={logo} alt="BetterIDEa" width={30} height={30} />
+            <div className="text-xl font-serif-display">BetterIDEa</div>
+          </div>
+        </div>
+        <Link href="mailto:hello@betteridea.dev" target="_blank">
+          <div className="flex gap-2 items-center hover:underline underline-offset-4">
+            <TbMailFilled size={20} /> hello@betteridea.dev
+          </div>
+        </Link>
+      </footer>
     </main>
   );
 }
